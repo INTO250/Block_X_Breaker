c6d9a5b978f831e8a00ec04f062b83cbfda1ffa1
Ken2525at <82943237+Ken2525at@users.noreply.github.com>
2021-05-24 20:58:13 +0800

    V0.12
    主菜单重制，选关界面添加返回按钮

--- Classes/StageSelectScene.cpp
+++ Classes/StageSelectScene.cpp
@@ -19,13 +19,26 @@ bool StageSelect::init()
     worldMap->setAnchorPoint(Vec2(0, 0));
     worldMap->setPosition(Vec2(0, 0));
     this->addChild(worldMap, -5);
-    auto stage_1 = MenuItemImage::create("stage_1.png", "stage_1_selected.png", CC_CALLBACK_1(StageSelect::stage_select, this));
-    auto stages = Menu::create(stage_1, NULL);
-    stages->setPosition(visibleSize.width / 2, visibleSize.height / 2);
-    this->addChild(stages, -1);
+    auto stage_1 = MenuItemImage::create("stage_1.png", "stage_1_selected.png", CC_CALLBACK_1(StageSelect::stage_1_select, this));
+    auto stage_1_menu = Menu::create(stage_1, NULL);
+    stage_1_menu->setPosition(visibleSize.width / 2, visibleSize.height / 2);
+
+    auto closeButton = MenuItemImage::create("close_button.png", "close_button_selected.png", CC_CALLBACK_1(StageSelect::backToMenu, this));
+    auto closeButtonMenu = Menu::create(closeButton, NULL);
+    closeButtonMenu->setAnchorPoint(Vec2(0, 1));
+    closeButtonMenu->setPosition(Vec2(50, visibleSize.height - 50));
+
+   
+    this->addChild(stage_1_menu, -1);
+    this->addChild(closeButtonMenu, -1);
+   
     return true;
 }
-void StageSelect::stage_select(cocos2d::Ref* pSender)
+void StageSelect::stage_1_select(cocos2d::Ref* pSender)
 {
-    Director::getInstance()->pushScene(TransitionFade::create(2.0f, GameScene::createScene(0)));
+    Director::getInstance()->pushScene(TransitionFade::create(2.0f, GameScene::createScene(1)));
+}
+void StageSelect::backToMenu(cocos2d::Ref* pSender)
+{
+    Director::getInstance()->replaceScene(TransitionFade::create(2.0f, MenuScene::createScene()));
 }
--- Classes/StageSelectScene.h
+++ Classes/StageSelectScene.h
@@ -2,13 +2,15 @@
 #ifndef  _STAGE_SELECT_H_
 #define  _STAGE_SELECT_H_
 #include "cocos2d.h"
+#include "MenuScene.h"
 
 class StageSelect : public cocos2d::Scene
 {
 public:
     static cocos2d::Scene* createScene();
     virtual bool init();
-    void stage_select(Ref* pSender);
+    void stage_1_select(cocos2d::Ref* pSender);
+    void backToMenu(cocos2d::Ref* pSender);
     CREATE_FUNC(StageSelect);
 };
 #endif
